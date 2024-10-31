14ac6c07c8ecb3e196c280924f0c97f4c3b13619
GreenfourmR <Monhbayr2400@gmail.com>
2024-10-31 12:51:58 +0800

    back-end update

--- back-end/server.js
+++ back-end/server.js
@@ -7,9 +7,33 @@
 // });
 // server.listen(3333);
 import express from "express";
+import bodyParser from "body-parser";
 
-const Port = 3939;
 const app = express();
+app.use(bodyParser.json());
+const Port = 3939;
+
+let users = [
+  { name: "tuugii", age: 32 },
+  { name: "toogii", age: 22 },
+];
+
+app.get("/users", (request, response) => {
+  response.send(users);
+});
+app.post("/users", (request, response) => {
+  const newUser = request.body;
+  users.push(newUser);
+  response.send(users);
+});
+app.put("/users", (request, response) => {
+  response.send("im put");
+});
+app.delete("/users", (request, response) => {
+  const { name } = request.body;
+  users = users.filter((user) => user.name !== name);
+  response.send(users);
+});
 
 app.listen(Port, () => {
   console.log("ajillaj ehellee");
